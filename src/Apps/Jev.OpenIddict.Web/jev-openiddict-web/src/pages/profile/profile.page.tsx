218005b911b87e34a240a9edd41964e3834e21b6
Terrance Jevon <terrance@hoa.co.za>
2022-04-30 20:57:54 +0200

    Chore: Fix typo

--- src/Apps/Jev.OpenIddict.Web/jev-openiddict-web/src/pages/profile/profile.page.tsx
+++ src/Apps/Jev.OpenIddict.Web/jev-openiddict-web/src/pages/profile/profile.page.tsx
@@ -3,6 +3,6 @@ import { Component, ReactElement } from 'react';
 
 export class ProfilePage extends Component {
   render(): ReactElement {
-    return <p>PROFILE PAGE BITCH</p>;
+    return <p>PROFILE PAGE</p>;
   }
 }
